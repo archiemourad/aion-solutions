caf71d1fbb9ef184eb44474dbe7b17891fceca97
Archie Mourad <mouradarchie+github@gmail.com>
2024-05-19 09:56:30 +1000

    added 'banner.mp4' and video to 'page.tsx'

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
 export default function Home() {
-  return <main />;
+  return (
+    <main>
+      <video
+        className="object-cover w-dvw h-dvh"
+        preload="none"
+        autoPlay
+        muted
+        loop
+      >
+        <source src="/banner.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </main>
+  );
 }
