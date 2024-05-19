a0385d24c6637c419d413868db35ada4c9fe5d19
Archie Mourad <mouradarchie+github@gmail.com>
2024-05-19 10:38:08 +1000

    styling, font to 'Adamina' and added 'host' title to 'page.tsx'

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,18 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
 export default function Home() {
+  const [host, setHost] = useState<string>();
+
+  useEffect(() => {
+    setHost(window.location.host);
+  }, []);
+
   return (
     <main>
       <video
-        className="object-cover w-dvw h-dvh"
+        className="object-cover w-dvw h-dvh brightness-50"
         preload="none"
         autoPlay
         muted
@@ -11,6 +21,10 @@ export default function Home() {
         <source src="/banner.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
+
+      <div className="absolute bottom-0 left-0 p-16">
+        <h1 className="md:text-3xl text-xl">{host}</h1>
+      </div>
     </main>
   );
 }
